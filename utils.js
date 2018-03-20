44c7133c29ae4b903edb52424509ab579dae0388
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-20 12:15:48 -0500

    add utils.js (missed in earlier commit)

---
+++ utils.js
@@ -0,0 +1,59 @@
+export const _0 = [
+  " _ ",
+  "| |",
+  "|_|",
+].join('')
+
+export const _1 = [
+  "   ",
+  "  |",
+  "  |",
+].join('')
+
+export const _2 = [
+  " _ ",
+  " _|",
+  "|_ ",
+].join('')
+
+export const _3 = [
+  " _ ",
+  " _|",
+  " _|",
+].join('')
+
+export const _4 = [
+  "   ",
+  "|_|",
+  "  |",
+].join('')
+
+export const _5 = [
+  " _ ",
+  "|_ ",
+  " _|",
+].join('')
+
+export const _6 = [
+  " _ ",
+  "|_ ",
+  "|_|",
+].join('')
+
+export const _7 = [
+  " _ ",
+  "  |",
+  "  |",
+].join('')
+
+export const _8 = [
+  " _ ",
+  "|_|",
+  "|_|",
+].join('')
+
+export const _9 = [
+  " _ ",
+  "|_|",
+  " _|",
+].join('')
