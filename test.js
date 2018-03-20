06d3df384c8d7d06b01ee0ddbdc33326fe3a32c7
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-03-20 09:57:35 -0500

    add test for 111111111

--- test.js
+++ test.js
@@ -9,4 +9,13 @@ describe('number scanner', () => {
     ].join('\n')
     expect(parse(input)).toEqual('000000000')
   })
+
+  it.skip('can recognize ones', () => {
+    const input = [
+      '                           ',
+      '  |  |  |  |  |  |  |  |  |',
+      '  |  |  |  |  |  |  |  |  |',
+    ].join('\n')
+    expect(parse(input)).toEqual('111111111')
+  })
 })
